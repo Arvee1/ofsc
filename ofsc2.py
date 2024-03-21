9e6c9e054e97b467abce087f7a9be86e90f5e751
Arvee1 <160126772+Arvee1@users.noreply.github.com>
2024-03-21 22:13:49 +1100

    Update ofsc2.py

--- ofsc2.py
+++ ofsc2.py
@@ -1,13 +1,4 @@
 import streamlit as st
-
-# Add custom CSS to hide the GitHub icon
-hide_github_icon = """
-#GithubIcon {
-  visibility: hidden;
-}
-"""
-st.markdown(hide_github_icon, unsafe_allow_html=True)
-
 __import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
