ea396b015bc2ec8d97f98200760fe862e43f544a
Arvee1 <160126772+Arvee1@users.noreply.github.com>
2024-03-21 22:15:47 +1100

    Update ofsc2.py

--- ofsc2.py
+++ ofsc2.py
@@ -1,4 +1,18 @@
 import streamlit as st
+
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 __import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
