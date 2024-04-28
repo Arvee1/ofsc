37833abcea118a8b04295475d035429f71aadc62
Arvee1 <160126772+Arvee1@users.noreply.github.com>
2024-04-28 21:47:31 +1000

    Update ofsc3.py

--- ofsc3.py
+++ ofsc3.py
@@ -9,6 +9,8 @@ import chromadb
 from chromadb.utils import embedding_functions
 import speech_recognition as sr
 import replicate
+import pyaudio
+import wave
 
 # initialize
 r = sr.Recognizer()
@@ -90,29 +92,58 @@ if st.button("Submit to AI", type="primary"):
      st.write(response.choices[0].message.content)
 
 if st.button("Say something", type="primary"):
-    with sr.Microphone() as source:
-        # clear background noise
-        r.adjust_for_ambient_noise(source, duration=0.3)
+     chunk = 1024  # Record in chunks of 1024 samples
+     sample_format = pyaudio.paInt16  # 16 bits per sample
+     channels = 2
+     fs = 44100  # Record at 44100 samples per second
+     seconds = 3
+     filename = "output.wav"
+     
+     p = pyaudio.PyAudio()  # Create an interface to PortAudio
+     
+     print('Recording')
+     
+     stream = p.open(format=sample_format,
+                     channels=channels,
+                     rate=fs,
+                     frames_per_buffer=chunk,
+                     input=True)
+     
+     frames = []  # Initialize array to store frames
+     
+     # Store data in chunks for 3 seconds
+     for i in range(0, int(fs / chunk * seconds)):
+         data = stream.read(chunk)
+         frames.append(data)
+     
+     # Stop and close the stream
+     stream.stop_stream()
+     stream.close()
+     # Terminate the PortAudio interface
+     p.terminate()
+     
+     print('Finished recording')
+     
+     # Save the recorded data as a WAV file
+     wf = wave.open(filename, 'wb')
+     wf.setnchannels(channels)
+     wf.setsampwidth(p.get_sample_size(sample_format))
+     wf.setframerate(fs)
+     wf.writeframes(b''.join(frames))
+     wf.close()
 
-        st.write("Speak now")
-        # capture the audio
-        audio = r.listen(source, timeout=5)
-
-    with open("microphone-results.wav", "wb") as f:
-        f.write(audio.get_wav_data())
-
-    soundfile = open("microphone-results.wav", "rb")
-    text = replicate.run(
-        "vaibhavs10/incredibly-fast-whisper:3ab86df6c8f54c11309d4d1f930ac292bad43ace52d10c80d87eb258b3c9f79c",
-        input={
+     soundfile = open("output.wav.wav", "rb")
+     text = replicate.run(
+          "vaibhavs10/incredibly-fast-whisper:3ab86df6c8f54c11309d4d1f930ac292bad43ace52d10c80d87eb258b3c9f79c",
+          input={
             "task": "transcribe",
             "audio": soundfile,
             "language": "None",
             "timestamp": "chunk",
             "batch_size": 64,
             "diarise_audio": False
-        }
-    )
+          }
+     )
      
     st.write("what you said: " + text['text'])
     prompt = text['text']
