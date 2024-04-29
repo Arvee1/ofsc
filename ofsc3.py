90afebfbe92d69568a83b7c6bc0f74da6cbfaccf
Arvee1 <160126772+Arvee1@users.noreply.github.com>
2024-04-29 13:26:15 +1000

    Update ofsc3.py

--- ofsc3.py
+++ ofsc3.py
@@ -80,7 +80,7 @@ if st.button("Submit to AI", type="primary"):
                 "top_p": 1,
                 "prompt": augment_query,
                 "temperature": 0.75,
-                "system_prompt": "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.\n\nIf a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.",
+                "system_prompt": "You are a helpful, respectful and honest assistant. Your answers will be concise.",
                 "length_penalty": 1,
                 "max_new_tokens": 800,
                 "prompt_template": "<s>[INST] <<SYS>>\n{system_prompt}\n<</SYS>>\n\n{prompt} [/INST]",
